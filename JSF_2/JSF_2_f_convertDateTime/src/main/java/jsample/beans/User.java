5ac52f769e7e7a189a9d090d4fc420e6405f10af
vikas.sachdeva <techie.vikas.sachdeva@gmail.com>
2017-01-02 03:40:57 +0900

    JSF 2 f:convertDateTime

---
+++ JSF_2/JSF_2_f_convertDateTime/src/main/java/jsample/beans/User.java
@@ -0,0 +1,24 @@
+package jsample.beans;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean(name = "user")
+@SessionScoped
+public class User implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Date dob;
+
+	public Date getDob() {
+		return dob;
+	}
+
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+}
