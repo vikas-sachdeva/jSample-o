eacd317aabf7183787d75661b06247423b4c4678
vikas.sachdeva <techie.vikas.sachdeva@gmail.com>
2017-03-13 19:02:33 +0900

    guava dependency added

--- Gradle/Gradle_Web_Application/src/main/java/jsample/servlet/HelloWorld.java
+++ Gradle/Gradle_Web_Application/src/main/java/jsample/servlet/HelloWorld.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.common.base.Joiner;
+
 public class HelloWorld extends HttpServlet {
 
 	private static final long serialVersionUID = -4481720201981492955L;
@@ -15,10 +17,6 @@ public class HelloWorld extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		PrintWriter out = resp.getWriter();
-		out.println("<html>");
-		out.println("<body>");
-		out.println("<h2>Hello Servlet !!!</h2>");
-		out.println("</body>");
-		out.println("</html>");
+		out.print(Joiner.on("\n").join("<html>", "<body>", "<h2>Hello Servlet !!!</h2>", "</body>", "</html>"));
 	}
 }
