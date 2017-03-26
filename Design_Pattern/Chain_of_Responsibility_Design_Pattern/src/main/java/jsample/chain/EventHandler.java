12b773272f4c66688d2a3755656468b5402dfcf5
vikas.sachdeva <techie.vikas.sachdeva@gmail.com>
2017-03-26 20:36:14 +0900

    commend added

--- Design_Pattern/Chain_of_Responsibility_Design_Pattern/src/main/java/jsample/chain/EventHandler.java
+++ Design_Pattern/Chain_of_Responsibility_Design_Pattern/src/main/java/jsample/chain/EventHandler.java
@@ -5,6 +5,14 @@ package jsample.chain;
  * pattern.
  */
 
+/**
+ * Chain of responsibility pattern is used to achieve loose coupling in software
+ * design where a request from client is passed to a chain of objects to process
+ * them. Then the object in the chain will decide whether it needs to process
+ * the request or not and whether the request should be sent to the next object
+ * in the chain or not.
+ *
+ */
 public interface EventHandler {
 
 	public void setNextEventHandler(EventHandler nextEventHandler);
