86e716a6e80d182449c219ed3c843b3a65eaf5db
vikas.sachdeva <techie.vikas.sachdeva@gmail.com>
2017-09-29 19:29:24 +0900

    Maven sort pom plugin sample

---
+++ Maven/Maven_SortPom_Plugin/src/main/java/jsample/App.java
@@ -0,0 +1,13 @@
+package jsample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
