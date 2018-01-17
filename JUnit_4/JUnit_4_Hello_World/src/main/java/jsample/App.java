83c6ecd9bffc13983a6df4924117957874b11c2b
vikas.sachdeva <techie.vikas.sachdeva@gmail.com>
2018-01-17 10:39:58 +0900

    migrated to new repo

--- JUnit_4/JUnit_4_Hello_World/src/main/java/jsample/App.java
+++
@@ -1,9 +0,0 @@
-package jsample;
-
-public class App {
-	public static void main(String[] args) {
-		HelloWorld helloWorld = new HelloWorld();
-		helloWorld.setName("JUnit 4");
-		System.out.println(helloWorld.getMessage());
-	}
-}
